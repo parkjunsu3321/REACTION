3e6a4b94cd07b839883f3fa35feefd59104f9a91
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-08 23:36:43 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,26 @@ import React, { useState, useEffect } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    // 여기에서 API 호출 또는 다른 작업 수행
-  }, []);
+useEffect(() => {
+    // API 호출
+    fetch('/api/hello')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            console.log(data);
+            setMessage(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            // 추가 디버깅을 위해 서버 응답을 콘솔에 출력
+            return error.text().then(errorMessage => console.error('Server response:', errorMessage));
+        });
+}, []);
+
 
   return (
     <div className="App">
