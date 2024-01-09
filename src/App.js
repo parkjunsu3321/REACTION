fdc48a4b28d0b4206dacd881a112db758ac6fa10
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 16:02:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     // axios를 이용한 API 호출
-    axios.get(process.env.REACT_APP_WAITLIST_API_URL+'/api/hello')
+    axios.get(process.env.REACT_APP_WAITLIST_API_URL+'/api')
       .then(response => {
         console.log(response.data);
         setMessage(response.data);
