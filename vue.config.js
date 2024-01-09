c2a6b9acd7a5aaab684f5d794d478dea5a9cb854
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 15:44:07 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
                 target: REACT_APP_WAITLIST_API_URL,
                 changeOrigin: true,
                 pathRewrite: {
-                    '^/api': '/store/db.php',
+                    '^/api': '',
                 },
             },
         },
