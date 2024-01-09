8f53d1869116911c5c8bad9b9a1e512e4353c849
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 23:50:25 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    // axios를 이용한 API 호출
     axios.get(process.env.REACT_APP_WAITLIST_API_URL+'/api')
       .then(response => {
         console.log(response.data);
@@ -20,6 +19,21 @@ function App() {
       });
   };
 
+const btndata = () => {
+    axios.get(process.env.REACT_APP_WAITLIST_API_URL+'/api/test')
+      .then(response => {
+        console.log(response.data);
+        setMessage(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        // 추가 디버깅을 위해 서버 응답을 콘솔에 출력
+        if (error.response) {
+          console.error('Server response:', error.response.data);
+        }
+      });
+  };
+  
   useEffect(() => {
     // 초기 렌더링 시 데이터 가져오기
     fetchData();
@@ -28,7 +42,7 @@ function App() {
   return (
     <div className="App">
       <h1>{message}</h1>
-      <button onClick={fetchData}>Fetch Data</button>
+      <button onClick={btndata}>Fetch Data</button>
     </div>
   );
 }
