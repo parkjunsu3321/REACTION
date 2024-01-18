42741cc9a7c78da454fc1be6d2c381c597bca89c
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-18 13:02:27 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,9 +20,27 @@ function App() {
       });
   };
 
+   const exitwindow = () => {
+    axios.get(process.env.REACT_APP_WAITLIST_API_URL + '/api/exit');
+  };
   
-  
-
+  const deleteItem = () => {
+    axios.delete(process.env.REACT_APP_WAITLIST_API_URL + '/api/delete', null, {
+    params: {
+      id: inputValueId,
+    }
+  })
+  .then(response => {
+    console.log(response.data);
+    setMessage(response.data);
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+    if (error.response) {
+      console.error('Server response:', error.response.data);
+      }
+    });
+  };
   
   
   const btndata = () => {
