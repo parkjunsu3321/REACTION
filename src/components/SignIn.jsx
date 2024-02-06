4ff4d51ce207222a6fe89033807fb40bb04ed139
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-06 18:32:28 +0900

    Update SignIn.jsx

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'; // useHistory 추가
 import '../styles/SignIn.css';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
+
 const User = {
   // 가상의 사용자 데이터 (임시로 추가) 삭제해도 댐
   // 로그인 시 스크립트 창 볼려고 만든거임
@@ -10,33 +11,27 @@ const User = {
 }
 
 export default function SignIn() {
-  // 아이디, 비밀번호 변수 선언
   const [userid, setId] = useState('');
   const [pw, setPw] = useState('');
-  
-  // 전체 유효성 검사를 위한 변수임
-  const [notAllow, setNotAllow] = useState(true); // true이면 로그인 버튼 비활성화 false 일 경우 활성화
+  const [notAllow, setNotAllow] = useState(true);
+  const history = useHistory(); // useHistory hook 추가
 
-  // 아이디 입력 핸들러임
   const handleId = (e) => {
     const newId = e.target.value;
     setId(newId);
     updateButtonState(newId, pw);
   };
 
-  // 비밀번호 입력 핸들러임
   const handlePassword = (e) => {
     const newPw = e.target.value;
     setPw(newPw);
     updateButtonState(userid, newPw);
   };
 
-  // 아이디와 비밀번호를 1자 이상이라도 입력 시 버튼 활성화
   const updateButtonState = (newId, newPw) => {
     setNotAllow(!(newId.length >= 1 && newPw.length >= 1));
   };
 
-  // 로그인 버튼 클릭 핸들러임
   const onClickConfirmButton = () => {
     axios.post(process.env.REACT_APP_WAITLIST_API_URL + '/api/login', {
       id: userid,
@@ -47,7 +42,12 @@ export default function SignIn() {
       },
     })
       .then(response => {
-          alert(response.data)
+        if (response.data === true) {
+          // 응답 데이터가 true이면 Layout 페이지로 이동
+          history.push('/Layout');
+        } else {
+          alert('로그인에 실패했습니다.');
+        }
       })
       .catch(error => {
         alert('Error fetching data:', error);
@@ -57,7 +57,6 @@ export default function SignIn() {
       });
   }
 
-  // 엔터 키 다운 가능하게 만든 핸들러임
   const handleKeyDown = (e) => {
     if (e.key === 'Enter' && !notAllow) {
       onClickConfirmButton();
