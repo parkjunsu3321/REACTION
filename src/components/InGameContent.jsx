2258223aacbdb33aa57efa4900733816441188df
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-17 21:33:10 +0900

    Update InGameContent.jsx

--- src/components/InGameContent.jsx
+++ src/components/InGameContent.jsx
@@ -183,7 +183,7 @@ const InGameContent = () => {
     const currentSong = popularSongs[currentIndex];
     var tag  = "hiphop";
     var time = 2.0;
-    setAnswer(currentSong.answer);
+    setAnswer(popularSongs[currentIndex].answer);
     const flaskUrl = 'https://port-0-flask-9zxht12blqjml81v.sel4.cloudtype.app/embedding';
     // GET 요청을 보낼 쿼리 파라미터 설정
   alert(answer);
