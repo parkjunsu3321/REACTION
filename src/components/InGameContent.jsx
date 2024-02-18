a10a8ff811fdbc15a62b93ca1ec491b0da194197
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-18 20:41:41 +0900

    Update InGameContent.jsx

--- src/components/InGameContent.jsx
+++ src/components/InGameContent.jsx
@@ -188,7 +188,6 @@ const InGameContent = () => {
     const flaskUrl = 'https://port-0-flask-9zxht12blqjml81v.sel4.cloudtype.app/embedding';
     // GET 요청을 보낼 쿼리 파라미터 설정
     console.log("Current answer:", tag);
-    handlePassClick();
     axios.get(flaskUrl, {
   params: {
     answer: answer[0],
