6e270ad69c38877f33e7042927c32ff949961290
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-02-27 22:43:20 +0900

    Update Ranking.js

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -428,7 +428,7 @@ const Ranking = () => {
                             <MobileMainBb/>
 
                             <MobileMainBc>
-                            <h1>{gameResults.length > 0 ? gameResults[0].game_result_player_id : ''}</h1>
+                            <h1>{gameResults[0].game_result_player_id}</h1>
                             </MobileMainBc>
                         </MobileMainB>
 
